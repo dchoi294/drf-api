8be4444900874cf56205110724b02ac9d2d70d91
dchoi294 <dchoi294@gmail.com>
2023-01-30 18:47:43 -0800

    progress

---
+++ practice/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from .serializers import PracticeSerializer
+from .models import Practice
+
+
+class PracticeList(generics.ListCreateAPIView):
+    # A QuerySet represents a collection of objects from your database. It can have zero, one or many filters.
+    # Filters narrow down the query results based on the given parameters. In SQL terms, a QuerySet equates to a
+    # SELECT statement, and a filter is a limiting clause such as WHERE or LIMIT. The queryset that should be used
+    # for returning objects from this view.
+    queryset = Practice.objects.all()
+    serializer_class = PracticeSerializer
+
+
+class PracticeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Practice.objects.all()
+    serializer_class = PracticeSerializer
